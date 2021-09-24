851e611f28d47854a9eccf6230362f3ead7ab545
Taekhun <nth9708@naver.com>
2021-09-25 00:12:37 +0900

    Rename: firebase rename

--- src/service/firebase.ts
+++ src/service/firebase.ts
@@ -4,8 +4,6 @@ const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
-  // storageBucket: 'univ-dorm-community.appspot.com',
-  // messagingSenderId: '733669720752',
 };
 
 const app = initializeApp(firebaseConfig);
