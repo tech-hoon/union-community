ee6e5c321d00b4ab0c23cf638c27919486cefa05
Taekhun <nth9708@naver.com>
2021-10-04 23:38:11 +0900

    Feat: post form 유효성 검증 util 추가

--- src/hooks/usePostForm.tsx
+++ src/hooks/usePostForm.tsx
@@ -1,6 +1,10 @@
 import { useState, RefObject } from 'react';
 import { useHistory } from 'react-router';
+import { useRecoilValue } from 'recoil';
+import { dbService } from 'service/firebase';
+import { loginUserState } from 'store/loginUser';
 import { PostType } from 'types';
+import { checkPostValidation } from 'utils/validation';
 
 interface Props {
   titleRef: RefObject<HTMLInputElement | null>;
@@ -11,15 +15,38 @@ interface Props {
 const usePostForm = ({ titleRef, categoryRef, contentRef }: Props) => {
   const history = useHistory();
   const [post, setPost] = useState<PostType>();
+  const loginUser = useRecoilValue(loginUserState);
 
   const onEditorCancle = () => window.confirm('글 작성을 취소하시겠습니까?') && history.push('/');
-  const onSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
+  const onSubmit: React.FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault();
-    setPost({
-      title: titleRef.current?.value!!,
-      category: categoryRef.current?.value!!,
-      content: contentRef.current?.state.value!!,
-    });
+
+    if (
+      checkPostValidation(
+        titleRef.current?.value!!,
+        categoryRef.current?.value!!,
+        contentRef.current?.state.value!!
+      )
+    ) {
+      await dbService.collection('posts').add({
+        category: categoryRef.current?.value!!,
+        title: titleRef.current?.value!!,
+        content: contentRef.current?.state.value!!,
+        creator: loginUser,
+        view_count: 0,
+        like_count: 0,
+        createdAt: new Date().toLocaleDateString(),
+        comment_list: [],
+      });
+
+      setPost({
+        title: titleRef.current?.value!!,
+        category: categoryRef.current?.value!!,
+        content: contentRef.current?.state.value!!,
+      });
+    } else {
+      window.alert('글을 모두 작성해주세요');
+    }
   };
 
   return {
--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,3 +1,9 @@
-export const checkValidation = (title: string, category: string, content: string) => {
-  return title && category && content;
+export const checkPostValidation = (
+  title: string | null,
+  category: string | null,
+  content: string
+) => {
+  const contentHTML = document.createElement('div');
+  contentHTML.innerHTML = content;
+  return Boolean(title && category && contentHTML.textContent);
 };
