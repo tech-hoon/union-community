86e8751c0e35657a8c0d66ed91683f457219e652
Taekhun <nth9708@naver.com>
2021-10-14 18:08:46 +0900

    Etc: 로그인 스텝 고정변수 분리

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -9,3 +9,9 @@ export const MENU_LIST = [
   { eng: 'study', kor: '스터디' },
   { eng: 'info', kor: '정보' },
 ];
+
+//Login Step
+export const SNS_LOGIN_STEP = 1;
+export const RESIDENT_AUTH_STEP = 2;
+export const NICKNAME_STEP = 3;
+export const LOGIN_DONE = 4;
