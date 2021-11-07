fbca8ec2bd1270479285ec24c64690c87572b361
Taekhun <nth9708@naver.com>
2021-11-07 23:48:55 +0900

    Etc: 주석 제거

--- src/api/count.ts
+++ src/api/count.ts
@@ -1,9 +1,7 @@
 import { dbService } from 'service/firebase';
 
 export const getUserPostCount = async () => {
-  // const userDB = await dbService.doc(`users/${uid}`).get();
-  // return userDB.data() ? userDB.data() : null;
-  const userCount = await (await dbService.collection('users').get()).size;
-  const postCount = await (await dbService.collection('posts').get()).size;
+  const userCount = (await dbService.collection('users').get()).size;
+  const postCount = (await dbService.collection('posts').get()).size;
   return { userCount, postCount };
 };
