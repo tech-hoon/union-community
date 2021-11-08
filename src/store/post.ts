7457a4067301f8fca11f5ca821ffd396ae64ea97
Taekhun <nth9708@naver.com>
2021-11-08 16:39:21 +0900

    Fix: 데이터 구조 변경

--- src/store/post.ts
+++ src/store/post.ts
@@ -1,20 +1,9 @@
 import { atom } from 'recoil';
 import { PostType } from 'types';
-import { PAGE_START } from 'utils/config';
-
-interface IgetPostsParams {
-  lastIndex: number;
-  category?: string;
-}
 
 export const postsState = atom<PostType[]>({
   key: 'posts',
   default: [],
-});
-
-export const postsPageIndex = atom<number>({
-  key: 'posts/pageIndex',
-  default: PAGE_START,
 });
 
 export const postsCategoryState = atom<string>({
@@ -25,9 +14,4 @@ export const postsCategoryState = atom<string>({
 export const postsOrderByState = atom<string>({
   key: 'posts/orderBy',
   default: 'created_at',
-});
-
-export const postsLastKey = atom<any>({
-  key: 'posts/lastKey',
-  default: '',
 });
--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,6 +1,6 @@
 export const ERROR_MESSAGE = 'Failed to fetch data, please check your API or internet connection';
-export const FIRST_INDEX = 6;
-export const CARD_AMOUNT = 6;
+export const PAGE_START = 1;
+export const CARD_LIMIT = 4;
 
 export const CATEGORY_LIST = [
   { eng: 'free', kor: '자유', color: '#18A0FB' },
