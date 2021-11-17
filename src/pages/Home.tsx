2e1963b318a308995be299c7dd5e8aa7c1e9a8f6
Taekhun <nth9708@naver.com>
2021-11-17 21:33:14 +0900

    Feat: 스크롤 위치 localStorage에 저장

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,53 +1,62 @@
-import { memo, useCallback } from 'react';
 import styled from 'styled-components';
 import Navbar from 'components/common/Navbar';
 import PostCardBox from 'components/Home/PostCardBox';
 import CategoryBox from 'components/Home/CategoryBox';
 import OrderbyBox from 'components/Home/OrderbyBox';
 import Footer from 'components/common/Footer';
-import useIntersectionObserver from 'hooks/useIntersectionObserver';
-import { useState, useRef, useEffect } from 'react';
+import useIntersectionObserver from 'hooks/post/useIntersectionObserver';
+import { useState, useRef, useEffect, useLayoutEffect, memo } from 'react';
 import CardSkeleton from 'components/common/Skeletons/CardSkeleton';
-import { useGetPosts } from 'hooks/useGetPosts';
 import SearchBox from 'components/Home/SearchBox';
-import { Refresh } from '@styled-icons/foundation';
 import { dbService } from 'service/firebase';
-import useDidUpdateEffect from 'hooks/useDidUpdateEffect';
+import { useGetPosts } from 'hooks/post/useGetPosts';
+import useLocalStorage from 'hooks/common/useLocalStorage';
+import { useLocation } from 'react-router';
 
 interface Props {}
 
 const Home = (props: Props) => {
-  const { posts, fetchPosts, fetchMorePosts, isLoading, isLastPost } = useGetPosts();
-  const [isUpdated, setIsUpdated] = useState(true);
-  const [isIntersecting, setIsIntersecting] = useState(false);
-
+  const { posts, updatePosts, fetchMorePosts, isLoading, lastVisiblePost } = useGetPosts();
+  const location = useLocation();
+  const [isUpdated, setIsUpdated] = useState(false);
   const ioRef = useRef<HTMLDivElement | null>(null);
   const entry = useIntersectionObserver(ioRef, {});
 
+  const [scrollY, setScrollY] = useLocalStorage('scrollY', 0);
+
   const onRefreshClick = () => {
-    fetchPosts();
     setIsUpdated(false);
+    updatePosts();
+    window.scroll({ behavior: 'smooth', top: 0 });
   };
 
-  useDidUpdateEffect(() => {
+  useLayoutEffect(() => {
+    window.scrollTo({ top: scrollY });
+
     const unsubscribe = dbService.collection('posts').onSnapshot((snapshot) => {
       snapshot.docChanges().forEach((change) => {
+        //TODO: 조회수 갱신(수정) 때문에 error 생김
         if (change.type === 'modified') {
+          console.log('new post');
           setIsUpdated(true);
         }
       });
     });
 
-    isUpdated && unsubscribe();
-
     return () => {
       unsubscribe();
+      setScrollY(window.scrollY);
     };
   }, []);
 
   useEffect(() => {
+    // 새 글,수정,삭제시 새로운 게시물로 fetch
+    location.state && updatePosts();
+  }, []);
+
+  useEffect(() => {
     entry?.isIntersecting && fetchMorePosts();
-  });
+  }, [lastVisiblePost, entry]);
 
   return (
     <Wrapper>
@@ -57,9 +66,9 @@ const Home = (props: Props) => {
         <OrderbyBox />
         <SearchBox />
       </MidWrapper>
-      {isLoading ? <CardSkeleton /> : <PostCardBox posts={posts || []} />}
+      {isLoading ? <CardSkeleton /> : <PostCardBox posts={posts} />}
       {isUpdated && <RefreshButton onClick={onRefreshClick}>새 게시물</RefreshButton>}
-      {!isLastPost && <Observer ref={ioRef} />}
+      <Observer ref={ioRef} />
       <Footer />
     </Wrapper>
   );
@@ -90,7 +99,19 @@ const MidWrapper = styled.div`
 `;
 
 const RefreshButton = styled.button`
-  top: 25%;
+  @keyframes popUpAnimation {
+    0% {
+      opacity: 0;
+      top: -50%;
+    }
+
+    100% {
+      opacity: 1;
+      top: 20%;
+    }
+  }
+
+  top: -200%;
   width: 100px;
   height: 30px;
   position: fixed;
@@ -100,6 +121,8 @@ const RefreshButton = styled.button`
   transform: translate(-50%, -50%);
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 50px;
+
+  animation: popUpAnimation 0.5s ease 0s 1 normal forwards running;
 `;
 
 export default memo(Home);
