a4ff0d0e0fff3e56b1edb9a26ddb2cd341be31c0
Taekhun <nth9708@naver.com>
2021-11-22 16:28:11 +0900

    WIP: getPosts selector 캐싱 작업

--- src/store/post.ts
+++ src/store/post.ts
@@ -23,30 +23,32 @@ export const lastVisiblePostState = atom<string>({
   dangerouslyAllowMutability: true,
 });
 
-// export const getPostsSelector = selector({
-//   key: 'get/posts',
-//   get: async ({ get }) => {
-//     const orderBy = get(postsOrderByState);
-//     const category = get(postsCategoryState);
-//     const lastVisible = get(lastVisiblePostState);
-//     const prevPosts = get(postsState);
+export const getPostsSelector = selector({
+  key: 'get/posts',
+  get: async ({ get }) => {
+    const orderBy = get(postsOrderByState);
+    const category = get(postsCategoryState);
 
-//     if (!!lastVisible) {
-//       console.log('더 불러오긔');
-//       const newPosts: any = await getMorePosts({ orderBy, category, lastVisible });
-//       return [...prevPosts, ...newPosts];
-//     }
+    const { data, lastVisiblePost }: any = await getInitialPosts({ orderBy, category });
 
-//     console.log('처음 불러오기');
-//     const posts = await getInitialPosts({ orderBy, category });
-//     return posts;
-//   },
-// });
+    return { data, lastVisiblePost };
+  },
+});
 
-// export const lastVisibleSelector = selector({
-//   key: 'posts/lastVisible',
-//   get: ({ get }) => {
-//     const posts = get(postsState);
-//     return posts[posts.length - 1];
-//   },
-// });
+export const getMorePostsSelector = selector({
+  key: 'get/more_posts',
+  get: async ({ get }) => {
+    const orderBy = get(postsOrderByState);
+    const category = get(postsCategoryState);
+    const lastVisiblePost = get(lastVisiblePostState);
+
+    if (!!lastVisiblePost) {
+      const { data, __lastVisiblePost }: any = await getMorePosts({
+        orderBy,
+        category,
+        lastVisiblePost,
+      });
+      return { data, lastVisiblePost: __lastVisiblePost };
+    }
+  },
+});
