7faf703c05e250346517dcc45d2c671976944e25
Taekhun <nth9708@naver.com>
2021-11-25 15:37:09 +0900

    Feat: 게시물 좋아요 firestore-functions 트리거

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,7 +7,6 @@ const firestore = admin.firestore();
 // export const helloWorld = functions.https.onRequest((request: any, response: any) => {
 //   functions.logger.info('Hello logs!', { structuredData: true });
 //   response.send('Hello from Firebase!');
-// });
 
 export const postCreated = functions.firestore.document('posts/{postId}').onCreate(() => {
   firestore.doc(`counter/posts`).update({
@@ -20,5 +19,15 @@ export const postDeleted = functions.firestore.document('posts/{postId}').onDele
     count: admin.firestore.FieldValue.increment(-1),
   });
 });
+
+export const postLiked = functions.firestore
+  .document('posts/{postId/liker_list}')
+  .onUpdate((doc: any) => {
+    console.log(doc, doc.id, doc.data());
+
+    // firestore.doc(`users/${uid}`).update({
+    //   liker_list: admin.firestore.FieldValue.arrayUnion(postId),
+    // });
+  });
 
 // export const postViewUp = functions.https.onRequest((request:any, response:any)=>{});
