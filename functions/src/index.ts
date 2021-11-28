53da1a81e934478401a851b30844c9fd05ef6b12
Taekhun <nth9708@naver.com>
2021-11-28 19:31:29 +0900

    Feat: 사용자 등록 트리거시, 슬랙으로 메시지 전송

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,32 +1,43 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import SlackWebhook = require('@slack/client');
 import _ = require('lodash');
 
 admin.initializeApp();
 const firestore = admin.firestore();
+const IncomingWebhook = SlackWebhook.IncomingWebhook;
+const url = 'SLACK_WEBHOOK_URL';
+const Slack = new IncomingWebhook(url);
 
-export const postCreated = functions.firestore.document('posts/{postId}').onCreate((doc) => {
-  firestore.doc(`counter/posts`).update({
-    count: admin.firestore.FieldValue.increment(1),
+export const postCreated = functions
+  .region('asia-northeast3')
+  .firestore.document('posts/{postId}')
+  .onCreate((snapshot) => {
+    firestore.doc(`counter/posts`).update({
+      count: admin.firestore.FieldValue.increment(1),
+    });
+
+    firestore.doc(`users/${snapshot.data().creator.uid}`).update({
+      post_list: admin.firestore.FieldValue.arrayUnion(snapshot.id),
+    });
   });
 
-  firestore.doc(`users/${doc.data().creator.uid}`).update({
-    post_list: admin.firestore.FieldValue.arrayUnion(doc.id),
-  });
-});
+export const postDeleted = functions
+  .region('asia-northeast3')
+  .firestore.document('posts/{postId}')
+  .onDelete((snapshot) => {
+    firestore.doc(`counter/posts`).update({
+      count: admin.firestore.FieldValue.increment(-1),
+    });
 
-export const postDeleted = functions.firestore.document('posts/{postId}').onDelete((doc) => {
-  firestore.doc(`counter/posts`).update({
-    count: admin.firestore.FieldValue.increment(-1),
+    firestore.doc(`users/${snapshot.data().creator.uid}`).update({
+      post_list: admin.firestore.FieldValue.arrayRemove(snapshot.id),
+    });
   });
 
-  firestore.doc(`users/${doc.data().creator.uid}`).update({
-    post_list: admin.firestore.FieldValue.arrayRemove(doc.id),
-  });
-});
-
-export const postLiked = functions.firestore
-  .document('posts/{postId}')
+export const postLiked = functions
+  .region('asia-northeast3')
+  .firestore.document('posts/{postId}')
   .onUpdate((change: functions.Change<functions.firestore.QueryDocumentSnapshot>, context) => {
     const liker_list_before = change.before.data().liker_list;
     const liker_list_after = change.after.data().liker_list;
@@ -34,7 +45,6 @@ export const postLiked = functions.firestore
 
     // liker_list 변경이 없으면 리턴
     if (_.isEqual(liker_list_before, liker_list_after)) {
-      functions.logger.log('@ Is Equal');
       return;
     }
 
@@ -57,5 +67,44 @@ export const postLiked = functions.firestore
       like_list: admin.firestore.FieldValue.arrayRemove(postId),
     });
   });
+
+export const commentCreated = functions
+  .region('asia-northeast3')
+  .firestore.document('posts/{postId}/comments/{commentId}')
+  .onCreate((_, context) => {
+    const postId = context.resource.name.split('/')[6];
+    firestore.doc(`posts/${postId}`).update({
+      comment_count: admin.firestore.FieldValue.increment(1),
+    });
+  });
+
+export const commentDeleted = functions
+  .region('asia-northeast3')
+  .firestore.document('posts/{postId}/comments/{commentId}')
+  .onDelete((_, context) => {
+    const postId = context.resource.name.split('/')[6];
+    firestore.doc(`posts/${postId}`).update({
+      comment_count: admin.firestore.FieldValue.increment(-1),
+    });
+  });
+
+export const userCreated = functions
+  .region('asia-northeast3')
+  .firestore.document('users/{userId}')
+  .onCreate((snapshot) => {
+    const { email, name, resident_auth_image, uid } = snapshot.data();
+    Slack.send(
+      `
+      새로운 사용자가 등록되었습니다.
+      
+      아이디: ${uid}
+      이메일: ${email}
+      이름: ${name}
+      인증사진: ${resident_auth_image}
+      `
+    );
+  });
+
+// const test =functions.auth.user().onCreate()
 
 // export const postViewUp = functions.https.onRequest((request:any, response:any)=>{});
