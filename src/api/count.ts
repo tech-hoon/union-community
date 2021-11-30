fcbeebc7ac5645aa7873d7cd206b70d84ca4f572
Taekhun <nth9708@naver.com>
2021-11-30 17:11:01 +0900

    Fix: count 예외처리

--- src/api/count.ts
+++ src/api/count.ts
@@ -3,5 +3,10 @@ import { dbService } from 'service/firebase';
 export const getUserPostCount = async () => {
   const res = await dbService.collection('counter').get();
   const [post, user] = res.docs.map((doc) => doc.data());
+
+  if (!post || !user) {
+    return { userCount: 0, postCount: 0 };
+  }
+
   return { userCount: user.count, postCount: post.count };
 };
