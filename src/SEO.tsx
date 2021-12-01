875d8aedc1c7e8e397c6429ffaedb6eb59665a77
Taekhun <nth9708@naver.com>
2021-12-01 20:30:07 +0900

    Feat: og meta 태그 추가

---
+++ src/SEO.tsx
@@ -0,0 +1,17 @@
+import Helmet from 'react-helmet';
+
+const SEO = () => (
+  <Helmet>
+    <meta name='theme-color' content='#000000' />
+    <meta name='description' content='Web site created using create-react-app' />
+    <meta property='og:type' content='website' />
+    <meta property='og:url' content='https://uni-on.me/' />
+    <meta property='og:title' content='대학생 연합기숙사 커뮤니티, 유니온' />
+    <meta property='og:image' content='https://uni-on.me/og-image.png' />
+    <meta property='og:description' content='대학생 연합기숙사 입주생을 위한 커뮤니티입니다.' />
+    <meta property='og:site_name' content='union' />
+    <meta property='og:locale' content='kor-KR' />
+  </Helmet>
+);
+
+export default SEO;
