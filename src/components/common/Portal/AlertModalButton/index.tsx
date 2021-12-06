981f86a476292bcd25f02e3abd15090cafdaa00c
Taekhun <nth9708@naver.com>
2021-12-06 16:30:34 +0900

    Feat: 경고 모달창 구현

---
+++ src/components/common/Portal/AlertModalButton/index.tsx
@@ -0,0 +1,61 @@
+import styled from 'styled-components';
+
+interface Props {
+  title: string;
+  twoButton?: boolean;
+  onClose?: () => void;
+  onOkay?: () => void;
+}
+
+const AlertModalButton = ({ title, twoButton = false, onClose, onOkay }: Props) => {
+  return (
+    <Wrapper>
+      <Title>{title}</Title>
+      <ButtonBox>
+        {twoButton && <CancelButton onClick={onClose}>취소</CancelButton>}
+        <OkayButton onClick={onOkay}>확인</OkayButton>
+      </ButtonBox>
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+  width: 350px;
+  height: 200px;
+  padding: 32px 24px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 36px;
+
+  overflow: hidden;
+`;
+
+const Title = styled.h2`
+  font-size: 1rem;
+  font-weight: 600;
+`;
+
+const ButtonBox = styled.div`
+  display: flex;
+  gap: 12px;
+`;
+
+const Button = styled.button`
+  font-size: 1rem;
+  height: 2rem;
+  padding: 0 20px;
+  border-radius: 4px;
+`;
+
+const OkayButton = styled(Button)`
+  background-color: #262626;
+  color: white;
+`;
+const CancelButton = styled(Button)`
+  background-color: #e9ecef;
+  color: black;
+`;
+
+export default AlertModalButton;
