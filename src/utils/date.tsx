82e301bea101612820a6517f96e6094048a3625b
Taekhun <nth9708@naver.com>
2021-12-06 16:33:22 +0900

    Fix: date 표시법 변경

--- src/utils/date.tsx
+++ src/utils/date.tsx
@@ -3,16 +3,39 @@ export const toDateStringByFormating = (
   timeInclude = false,
   delimiter = '-'
 ) => {
-  const dateInstance = new Date(timestamp);
+  const currTime = new Date().getTime();
 
+  const timeDiff = (currTime - timestamp) / 1000;
+
+  const MIN = 60;
+  const HOUR = 60 * MIN;
+  const DAY = 24 * HOUR;
+  const MONTH = 31 * DAY;
+
+  if (timeDiff < 60) {
+    return `방금전`;
+  }
+
+  if (timeDiff < HOUR) {
+    return `${Math.floor(timeDiff / MIN)}분전`;
+  }
+
+  if (timeDiff < DAY) {
+    return `${Math.floor(timeDiff / HOUR)}시간전`;
+  }
+
+  if (timeDiff < MONTH) {
+    return `${Math.floor(timeDiff / DAY)}일전`;
+  }
+
+  const dateInstance = new Date(timestamp);
   const year = dateInstance.getFullYear();
   const month = dateInstance.getMonth() + 1;
   const date = dateInstance.getDate();
   const hours = dateInstance.getHours();
   const minutes = dateInstance.getMinutes();
-  const seconds = dateInstance.getSeconds();
 
   return `${[year, month, date].join(delimiter)}${
-    timeInclude === true ? ' ' + [hours, minutes, seconds].join(':') : ''
+    timeInclude === true ? ' ' + [hours, minutes].join(':') : ''
   }`;
 };
