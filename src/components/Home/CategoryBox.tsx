daf05730b01285aaca35ae0cbf63063bd2ae93de
Taekhun <nth9708@naver.com>
2021-12-08 17:16:44 +0900

    Style: 카테고리 모바일에선 스크롤

--- src/components/Home/CategoryBox.tsx
+++ src/components/Home/CategoryBox.tsx
@@ -1,21 +1,33 @@
 import { useRecoilState } from 'recoil';
-import { useState, memo } from 'react';
+import { memo, useLayoutEffect, useRef } from 'react';
 import { postsCategoryState } from 'store/post';
 import styled from 'styled-components';
 import { CATEGORY_LIST } from 'utils/config';
 import { categoryColor } from 'utils/categoryColor';
+import useLocalStorage from 'hooks/common/useLocalStorage';
 
 interface Props {}
 
 const CategoryBox = (props: Props) => {
   const [category, setCategory] = useRecoilState(postsCategoryState);
+  const [scrollX, setScrollX] = useLocalStorage('scrollX', 0);
+  const ref = useRef<HTMLDivElement>(null);
 
   const onMenuClick = (value: string) => {
     setCategory(value);
   };
 
+  useLayoutEffect(() => {
+    ref?.current?.scrollTo({ left: scrollX });
+
+    return () => {
+      const __scrollX = ref?.current?.scrollLeft;
+      setScrollX(__scrollX);
+    };
+  }, []);
+
   return (
-    <Wrapper>
+    <Wrapper ref={ref}>
       {CATEGORY_LIST.map(({ kor }, id) => (
         <Menu
           onClick={() => onMenuClick(kor)}
@@ -36,7 +48,8 @@ const Wrapper = styled.div`
   padding: 0px 60px 20px;
 
   display: flex;
-  gap: 1%;
+  justify-content: space-between;
+
   margin: 0px auto 12px;
 
   @media ${({ theme }) => theme.size.mobile} {
@@ -44,9 +57,14 @@ const Wrapper = styled.div`
     padding: 0;
     margin: 20px auto 28px;
     font-size: 1em;
+
+    overflow-x: auto;
   }
 
   user-select: none;
+  &::-webkit-scrollbar {
+    display: none;
+  }
 `;
 
 interface IMenu {
@@ -55,21 +73,31 @@ interface IMenu {
 
 const Menu = styled.button<IMenu>`
   width: 100%;
-  height: 36px;
+  height: 40px;
   border-radius: 16px;
   background-color: ${(props) => (props.isClicked ? props.color : '#fff')};
   color: ${(props) => (props.isClicked ? '#fff' : '#000')};
 
   box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.25);
-
   font-size: 1em;
   font-weight: 500;
+  margin: 2px;
+
   &:hover {
     color: white;
     background-color: ${(props) => props.color};
   }
+
   @media ${({ theme }) => theme.size.mobile} {
-    font-size: 0.8em;
+    height: 36px;
+    font-size: 0.9em;
+  }
+
+  @media ${({ theme }) => theme.size.mobileS} {
+    width: 60px;
+    height: 32px;
+    font-size: 0.75em;
+    flex: 0 0 auto;
   }
 `;
 
