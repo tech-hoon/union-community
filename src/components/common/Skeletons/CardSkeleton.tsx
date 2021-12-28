09368763dc0df9387bf4e591b060c8b41d42a2b2
Taekhun <nth9708@naver.com>
2021-12-28 19:48:41 +0900

    Style: PostCard Skeleton 크기 수정

--- src/components/common/Skeletons/CardSkeleton.tsx
+++ src/components/common/Skeletons/CardSkeleton.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import styled from 'styled-components';
 import SkeletonBar from './components/SkeletonBar';
 
@@ -24,41 +23,43 @@ const CardSkeleton = (props: Props) => {
 const Wrapper = styled.ul`
   max-width: 1120px;
   padding: 0 60px;
+  user-select: none;
 
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  gap: 32px;
-  margin: 40px auto;
+  grid-template-columns: repeat(3, minmax(0, 1fr));
+
+  gap: 30px;
+  margin: 30px auto;
 
   @media ${({ theme }) => theme.size.tablet} {
-    grid-template-columns: 1fr 1fr;
+    grid-template-columns: repeat(2, minmax(0, 1fr));
   }
 
   @media ${({ theme }) => theme.size.mobile} {
-    grid-template-columns: 1fr;
-    width: 70%;
+    grid-template-columns: repeat(1, minmax(0, 1fr));
+    width: 80%;
     padding: 0px;
   }
 `;
 
-const Card = styled.div`
-  width: 100%;
-  height: 260px;
+const Card = styled.li`
   padding: 24px;
   background: #ffffff;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.25);
   border-radius: 20px;
   display: flex;
   flex-direction: column;
   gap: 20px;
-  position: relative;
-  overflow: hidden;
+
+  @media ${({ theme }) => theme.size.desktop} {
+    height: 15rem;
+  }
 `;
 
 const Title = styled(SkeletonBar)``;
 
 const Content = styled(SkeletonBar)`
-  height: 120px;
+  height: 130px;
 `;
 
 const Bottom = styled.div`
