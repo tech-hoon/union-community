a0f89d93e23b3f7d62d7538a1cf2763fb4acec29
Taekhun <nth9708@naver.com>
2022-01-17 00:09:32 +0900

    Feat: new post listener

---
+++ src/hooks/common/useSessionStorage.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+const useSessionStorage = (key: string, initialValue: any) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = window.sessionStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      return initialValue;
+    }
+  });
+
+  const setValue = (value: string) => {
+    try {
+      setStoredValue(value);
+      window.sessionStorage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return [storedValue, setValue];
+};
+
+export default useSessionStorage;
