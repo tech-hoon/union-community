dfa003e912d1f9ede82b7afa16c4d73d479c5122
Taekhun <nth9708@naver.com>
2022-02-03 17:34:35 +0900

    Feat: image carousel slider

---
+++ src/components/common/Slider/index.tsx
@@ -0,0 +1,39 @@
+import { ReactNode } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Autoplay, Pagination, Navigation } from 'swiper';
+
+import 'swiper/swiper-bundle.min.css';
+import 'swiper/swiper.min.css';
+import 'swiper/components/navigation/navigation.min.css';
+import 'swiper/components/pagination/pagination.min.css';
+import './styles.css';
+
+interface IProps {
+  children: ReactNode[];
+}
+
+SwiperCore.use([Autoplay, Pagination, Navigation]);
+
+const Slider = ({ children }: IProps) => {
+  return (
+    <Swiper
+      slidesPerView={1}
+      autoplay={{
+        delay: 2000,
+        disableOnInteraction: false,
+        pauseOnMouseEnter: true,
+      }}
+      loop
+      pagination={{
+        clickable: true,
+      }}
+      navigation={true}
+    >
+      {children.map((child, key) => (
+        <SwiperSlide key={key}>{child}</SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+
+export default Slider;
