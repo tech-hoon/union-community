3dcb981570e2ebe7351fb747d8ec90ea0a40e216
Taekhun <nth9708@naver.com>
2022-02-24 23:17:56 +0900

    Chore: Icon 추가

---
+++ src/assets/icons/KebabMenuIcon.tsx
@@ -0,0 +1,9 @@
+const KebabMenuIcon = () => (
+  <svg width='3' height='15' viewBox='0 0 2 10' fill='none' xmlns='http://www.w3.org/2000/svg'>
+    <circle cx='1' cy='1' r='1' fill='#5C5C5C' />
+    <circle cx='1' cy='5' r='1' fill='#5C5C5C' />
+    <circle cx='1' cy='9' r='1' fill='#5C5C5C' />
+  </svg>
+);
+
+export default KebabMenuIcon;
---
+++ src/assets/icons/ReplyArrow.tsx
@@ -0,0 +1,13 @@
+const ReplyArrow = () => (
+  <svg width='11' height='17' viewBox='0 0 11 17' fill='none' xmlns='http://www.w3.org/2000/svg'>
+    <path
+      d='M7.46094 10.4766L10.0503 13.0633L7.46094 15.6527'
+      stroke='#5C5C5C'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <path d='M10.0498 13.0637H1V1' stroke='#5C5C5C' strokeLinecap='round' strokeLinejoin='round' />
+  </svg>
+);
+
+export default ReplyArrow;
