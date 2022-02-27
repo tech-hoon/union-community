3fd43bab2e92048e465651da2c14f7632244205a
Taekhun <nth9708@naver.com>
2022-02-27 15:09:10 +0900

    Fix: fix service worker life-cycle

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,36 +1,45 @@
+/* eslint-disable no-undef */
 /* eslint-disable no-restricted-globals */
 
-// 캐시 이름
-const CACHE_NAME = 'cache-v1';
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-// 캐싱할 파일
-const FILES_TO_CACHE = ['./offline.html', '/favicon.ico'];
+const CACHE = 'pwabuilder-page';
 
-// 상술한 파일 캐싱
-self.addEventListener('install', (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE)));
+const offlineFallbackPage = 'offline.html';
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
 });
 
-// CACHE_NAME이 변경되면 오래된 캐시 삭제
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keyList) =>
-      Promise.all(
-        keyList.map((key) => {
-          if (CACHE_NAME !== key) return caches.delete(key);
-        })
-      )
-    )
-  );
+self.addEventListener('install', async (event) => {
+  event.waitUntil(caches.open(CACHE).then((cache) => cache.add(offlineFallbackPage)));
 });
 
-// 요청에 실패하면 오프라인 페이지 표시
+if (workbox.navigationPreload.isSupported()) {
+  workbox.navigationPreload.enable();
+}
+
 self.addEventListener('fetch', (event) => {
-  if ('navigate' !== event.request.mode) return;
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      (async () => {
+        try {
+          const preloadResp = await event.preloadResponse;
 
-  event.respondWith(
-    fetch(event.request).catch(() =>
-      caches.open(CACHE_NAME).then((cache) => cache.match('./offline.html'))
-    )
-  );
+          if (preloadResp) {
+            return preloadResp;
+          }
+
+          const networkResp = await fetch(event.request);
+          return networkResp;
+        } catch (error) {
+          const cache = await caches.open(CACHE);
+          const cachedResp = await cache.match(offlineFallbackPage);
+          return cachedResp;
+        }
+      })()
+    );
+  }
 });
