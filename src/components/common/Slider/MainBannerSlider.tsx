c4ef24f29d1d03d48dcaebbf7effe38d46a5abac
Taekhun <nth9708@naver.com>
2022-03-05 23:51:50 +0900

    Fix: slider swiper error fix

--- src/components/common/Slider/MainBannerSlider.tsx
+++ src/components/common/Slider/MainBannerSlider.tsx
@@ -1,6 +1,4 @@
 import styled from 'styled-components';
-
-import { ReactNode } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore, { Autoplay, Pagination, Navigation } from 'swiper';
 import { MainBanner1M, MainBanner1L, MainBanner2M, MainBanner2L } from 'assets/banner';
@@ -11,57 +9,38 @@ import 'swiper/components/navigation/navigation.min.css';
 import 'swiper/components/pagination/pagination.min.css';
 import './styles.css';
 
-interface IProps {
-  children: ReactNode[];
-}
-
-const Slider = ({ children }: IProps) => {
-  return (
-    <Swiper
-      slidesPerView={1}
-      autoplay={{
-        delay: 4000,
-        disableOnInteraction: false,
-        pauseOnMouseEnter: true,
-      }}
-      loop
-      pagination={{
-        clickable: true,
-      }}
-      navigation={true}
-    >
-      {children.map((child, key) => (
-        <SwiperSlide key={key}>{child}</SwiperSlide>
-      ))}
-    </Swiper>
-  );
-};
-
 SwiperCore.use([Autoplay, Pagination, Navigation]);
 
-interface IProps {
-  children: ReactNode[];
-}
+const MainBannerSlider = () => (
+  <Swiper
+    slidesPerView={1}
+    autoplay={{
+      delay: 4000,
+      disableOnInteraction: false,
+    }}
+    observer={true}
+    observeParents={true}
+    loop
+    pagination={{
+      clickable: true,
+    }}
+    navigation={true}
+  >
+    <SwiperSlide>
+      <Picture>
+        <source media='(max-width:768px)' srcSet={MainBanner1M} />
+        <img src={MainBanner1L} alt='메인 배너1' />
+      </Picture>
+    </SwiperSlide>
 
-const MainBannerSlider = () => {
-  return (
-    <Slider>
-      <Swiper>
-        <Picture>
-          <source media='(max-width:768px)' srcSet={MainBanner1M} />
-          <img src={MainBanner1L} alt='메인 배너1' />
-        </Picture>
-      </Swiper>
-
-      <Swiper>
-        <Picture>
-          <source media='(max-width:768px)' srcSet={MainBanner2M} />
-          <img src={MainBanner2L} alt='메인 배너2' />
-        </Picture>
-      </Swiper>
-    </Slider>
-  );
-};
+    <SwiperSlide>
+      <Picture>
+        <source media='(max-width:768px)' srcSet={MainBanner2M} />
+        <img src={MainBanner2L} alt='메인 배너2' />
+      </Picture>
+    </SwiperSlide>
+  </Swiper>
+);
 
 const Picture = styled.picture`
   & > * {
