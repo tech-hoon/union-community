7ca25942256781dc316a86307cd8768a2b42105c
Taekhun <nth9708@naver.com>
2022-03-09 20:26:19 +0900

    Hotfix: iOS notch cover

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -10,6 +10,11 @@ const GlobalStyle = createGlobalStyle`
     -webkit-tap-highlight-color:transparent;
   }
 
+  html{
+    min-height: calc(100% + env(safe-area-inset-top));
+    padding: env(safe-area-inset-top) env(safe-area-inset-right) env(safe-area-inset-bottom) env(safe-area-inset-left);
+  }
+
   a {
     text-decoration-line:none;
     color:#2883f3;
