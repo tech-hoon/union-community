e7dc3997ea7c37d8458ee19548bba104ca81661c
Taekhun <nth9708@naver.com>
2022-03-09 18:02:22 +0900

    Hotfix: manifest display mode fix

--- src/components/common/Navbar/index.tsx
+++ src/components/common/Navbar/index.tsx
@@ -38,8 +38,6 @@ const Navbar = ({ children = <Aside />, option = 'home' }: IProps) => {
 const Wrapper = styled.div`
   background-color: #f8f9fa;
   position: sticky;
-  /* top: constant(safe-area-inset-bottom); */
-  /* top: env(safe-area-inset-top); */
   top: 0;
   z-index: 111;
   border-bottom: 1px solid #ededed;
