c42585c6dd6699af5d1bd29cc5fbf031febc13d2
Taekhun <nth9708@naver.com>
2022-03-26 22:23:13 +0900

    Fix: category color 예외처리

--- src/utils/categoryColor.ts
+++ src/utils/categoryColor.ts
@@ -1,7 +1,7 @@
 import { CATEGORY_LIST, PRODUCT_STATUS, PRODUCT_TYPE } from './config';
 
 export const categoryColor = (category: string) =>
-  CATEGORY_LIST.filter(({ kor }) => kor === category)[0].color;
+  CATEGORY_LIST.filter(({ kor }) => kor === category)[0]?.color || 'black';
 
 export const productTypeColor = (category: string) =>
   PRODUCT_TYPE.filter(({ kor }) => kor === category)[0].color;
