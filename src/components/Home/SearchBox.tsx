89d1803b5bdb151270a4d258b01caf71df00f556
Taekhun <nth9708@naver.com>
2022-03-27 04:54:56 +0900

    Etc: 기타 수정 사항

--- src/components/Home/SearchBox.tsx
+++ src/components/Home/SearchBox.tsx
@@ -6,39 +6,37 @@ import { useGetPosts } from 'hooks/post/useGetPosts';
 import useDebounce from 'hooks/common/useDebounce';
 
 const SearchBox = () => {
-  const [searchBy, setSearchBy] = useState<string>('contents');
-  const [value, setValue] = useState<string>('');
-  const { category, orderBy, setPosts } = useGetPosts();
-  const debouncedValue = useDebounce({ value: value, delay: 500 });
-
-  const onInputChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
-    setValue(e.target.value);
-  };
-  const onSelectChange: React.ChangeEventHandler<HTMLSelectElement> = (e) => {
-    setSearchBy(e.target.value);
-  };
-  const onSearchValue = async () => {
-    const _posts: any = await getPostBySearch(category, orderBy, searchBy, debouncedValue);
-    _posts && setPosts(_posts);
-  };
-
-  useEffect(() => {
-    debouncedValue && onSearchValue();
-  }, [debouncedValue]);
-
-  return (
-    <Wrapper>
-      <Select defaultValue='content' onChange={onSelectChange}>
-        <Option value='content'>내용</Option>
-        <Option value='creator'>작성자</Option>
-      </Select>
-      <Input onChange={onInputChange} />
-      <SearchIcon size='20px' onClick={onSearchValue} />
-    </Wrapper>
-  );
+  // const [searchBy, setSearchBy] = useState<string>('contents');
+  // const [value, setValue] = useState<string>('');
+  // const { category, orderBy, setPosts } = useGetPosts();
+  // const debouncedValue = useDebounce({ value: value, delay: 500 });
+  // const onInputChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+  //   setValue(e.target.value);
+  // };
+  // const onSelectChange: React.ChangeEventHandler<HTMLSelectElement> = (e) => {
+  //   setSearchBy(e.target.value);
+  // };
+  // const onSearchValue = async () => {
+  //   const _posts: any = await getPostBySearch(category, orderBy, searchBy, debouncedValue);
+  //   _posts && setPosts(_posts);
+  // };
+  // useEffect(() => {
+  //   // debouncedValue && onSearchValue();
+  // }, [debouncedValue]);
+  // return (
+  //   <Wrapper>
+  //     <Select defaultValue='content' onChange={onSelectChange}>
+  //       <Option value='content'>내용</Option>
+  //       <Option value='creator'>작성자</Option>
+  //     </Select>
+  //     <Input onChange={onInputChange} />
+  //     <SearchIcon size='20px' onClick={onSearchValue} />
+  //   </Wrapper>
+  // );
+  <div>hello</div>;
 };
 
-export default memo(SearchBox);
+export default SearchBox;
 
 const Wrapper = styled.div`
   display: flex;
